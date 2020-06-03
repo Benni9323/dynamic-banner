b9202f596cdc5d4b3318fd53660ca0d82dcf8d29
Benni9323 <benny187v@gmail.com>
2020-06-03 21:50:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
-$gif = new Imagick('banner.gif');
+$gif = new Imagick('banner.gif'); //Filename/Path to
 
-$drawColorHex = new ImagickPixel('#808080');
+$drawColorHex = new ImagickPixel('#808080'); //Change Color Hex Code here
 
 $draw = new ImagickDraw();    
-$draw->setFont('AmericanGothic-Bold.otf');
-$draw->setFontSize(80);
-$draw->setFillColor($drawColorHex);
-$draw->setFillOpacity(0.8);
+$draw->setFont('AmericanGothic-Bold.otf'); //Change Font
+$draw->setFontSize(80); //Change Font Size
+$draw->setFillColor($drawColorHex); //Fill the Text with the colorHex above
+$draw->setFillOpacity(0.8); //Set some opacity to the Text
 
-$countJsonUrl = "https://img.gcrp.cc/api/count.php";
+$countJsonUrl = "https://api.bennisengine.de/api"; //Replace Json Request Url here
 
-$responseJson = file_get_contents($countJsonUrl);
-$countData = json_decode($responseJson, true);
+$responseJson = file_get_contents($countJsonUrl); //Getting file contents
+$countData = json_decode($responseJson, true); //Just decode the file content to json
 
-foreach($gif as $frame){
-  $gif->annotateImage($draw, $x = 1155, $y = 200, $angle = 0, $countData["Players_ALTV"]);   
-  $gif->annotateImage($draw, $x = 1155, $y = 365, $angle = 0, $countData["Players_TS3"]);         
+foreach($gif as $frame){ //Setting text Every Frame of $gif
+  $gif->annotateImage($draw, $x = 1155, $y = 200, $angle = 0, $countData["players_game-altv"]); //Applying count Data to text 
+  $gif->annotateImage($draw, $x = 1155, $y = 365, $angle = 0, $countData["players_voip-ts3"]);  //Applying count Data to text       
 } 
 
    
 
-header('Content-Type: image/gif');
-print $gif->getImagesBlob();
+header('Content-Type: image/gif'); //Set Header for Final gif if website has been opended
+print $gif->getImagesBlob(); //Printing Image blobs
 ?>
